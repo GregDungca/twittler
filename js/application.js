7f240c42a685cc1932d94c426dadcfba8c67d22f
GregDungca <greg.dungca@gmail.com>
2015-09-15 16:37:09 -0400

    Added col bootstrap class and centred content

--- js/application.js
+++ js/application.js
@@ -1,16 +1,19 @@
 $(document).ready(function(){
-  debugger;
+  //debugger;
   var $body = $('.tweetList');
   //$body.html('');    
   var index = streams.home.length - 1;
   while(index >= 0){
-    var tweetContainer = '<div></div>';
     var tweet = streams.home[index];
-    //var $tweet = $('li');
+    var message = '<div>' + tweet.message + '</div>';
     var user = tweet.user;// need to insert a link for a modal for the user
+    var tweetContainer = '<div class="tweet-container">' + message + '</div>';
+    
+    //var $tweet = $('li');
+    
     //$tweet.text('@' + user + ': ' + tweet.message + ' at ' + tweet.created_at);
-    var message = '<li>' + tweet.message + '</li>';
-    $(message).appendTo($body);
+    
+    $(tweetContainer).prependTo($body);
     index -= 1;
   }  
 });
