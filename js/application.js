89b67c1b9b0b652473c0a135a6fefa408875f3c4
GregDungca <greg.dungca@gmail.com>
2015-09-15 15:32:32 -0400

    Fixed issue with tweets not displaying

--- js/application.js
+++ js/application.js
@@ -4,11 +4,13 @@ $(document).ready(function(){
   //$body.html('');    
   var index = streams.home.length - 1;
   while(index >= 0){
+    var tweetContainer = '<div></div>';
     var tweet = streams.home[index];
-    var $tweet = $('li');
+    //var $tweet = $('li');
     var user = tweet.user;// need to insert a link for a modal for the user
-    $tweet.text('@' + user + ': ' + tweet.message + ' at ' + tweet.created_at);
-    $tweet.appendTo($body);
+    //$tweet.text('@' + user + ': ' + tweet.message + ' at ' + tweet.created_at);
+    var message = '<li>' + tweet.message + '</li>';
+    $(message).appendTo($body);
     index -= 1;
   }  
 });
