a6231477f03398438d3b498ed7b26c440a107acb
GregDungca <greg.dungca@gmail.com>
2015-09-18 01:56:55 -0400

    Implemented checkNewTweets function, but still need to troubleshoot why it won't run at a set interval

--- js/application.js
+++ js/application.js
@@ -1,16 +1,22 @@
 var $body = $('.tweet-list');
+var index, updatedIndex;
 $(document).ready(function(){
   //$body.html('');    
-  var index = streams.home.length - 1;
+  index = streams.home.length - 1;
+ 
+  index = getNewTweets(0);
   debugger;
-  index = getNewTweets(index);
-  console.log(index);
+  setInterval(checkNewTweets(index), 1500);
+  $('button.show-more').on('click', function() {
+    $('div.show-more').hide();//optionally animate this
+    index = getNewTweets(updatedIndex, index);
+  });
 
-  var showMore = '<div class="tweet-container">' + '<button class="show-more">Show more</button>' + '</div>';
-  $(showMore).prependTo($body);
-  $('.show-more').on('click', function() {
-    getNewTweets(index);
-  })
+  // var showMore = '<div class="tweet-container">' + '<button class="show-more">Show more</button>' + '</div>';
+  // $(showMore).prependTo($body);
+  // $('.show-more').on('click', function() {
+  //   getNewTweets(index);
+  // })
 });
 
 
@@ -20,8 +26,8 @@ function extractTime(rawTime) {
 //   Thu Sep 17 2015 17:02:53 GMT-0400 (EDT)
 }
 
-function getNewTweets(index) {
-  while(index >= 0){
+function getNewTweets(endIndex) {
+  while(index >= endIndex){
     var tweet = streams.home[index];
     var user = '<span class="user"><strong>' + tweet.user + '</strong></span>';// need to insert a link for a modal for the user
     var messageTime = '<span class="message-time">' + extractTime(tweet.created_at) + '</span>';
@@ -40,3 +46,16 @@ function getNewTweets(index) {
   return streams.home.length - 1;
 
 } 
+
+function checkNewTweets(index) {
+  console.log('checkNewTweets');
+  var updatedIndex = streams.home.length - 1;
+  if (updatedIndex > index) {
+    var numNewTweets = updatedIndex - index;
+    if ($('div.show-more').css('display') === 'none') {
+      $('div.show-more').show();
+      $('.num-new-tweets').text(numNewTweets);
+    }
+  }
+
+}
