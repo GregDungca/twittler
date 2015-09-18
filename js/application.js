185d685e5b169c55d9571d9f603017fc1a93f20b
GregDungca <greg.dungca@gmail.com>
2015-09-18 14:22:39 -0400

    Implemented Show more tweets functionality

--- js/application.js
+++ js/application.js
@@ -1,15 +1,15 @@
 var $body = $('.tweet-list');
-var index, updatedIndex;
+var index1, index2;
 $(document).ready(function(){
   //$body.html('');    
-  index = streams.home.length - 1;
- 
-  index = getNewTweets(0);
-  debugger;
-  setInterval(checkNewTweets(index), 1500);
+  index1 = 0;
+  index2 = streams.home.length - 1;
+  getNewTweets();
+
+  setInterval(checkNewTweets, 15000);
   $('button.show-more').on('click', function() {
     $('div.show-more').hide();//optionally animate this
-    index = getNewTweets(updatedIndex, index);
+    getNewTweets();
   });
 
   // var showMore = '<div class="tweet-container">' + '<button class="show-more">Show more</button>' + '</div>';
@@ -26,36 +26,38 @@ function extractTime(rawTime) {
 //   Thu Sep 17 2015 17:02:53 GMT-0400 (EDT)
 }
 
-function getNewTweets(endIndex) {
-  while(index >= endIndex){
-    var tweet = streams.home[index];
+function getNewTweets() {
+  for ( var i = index1; i < index2; i ++ ) {
+    var tweet = streams.home[i];
     var user = '<span class="user"><strong>' + tweet.user + '</strong></span>';// need to insert a link for a modal for the user
     var messageTime = '<span class="message-time">' + extractTime(tweet.created_at) + '</span>';
     var message = '<div class="message">' + tweet.message + '</div>';
     var tweetContainer = '<div class="tweet-container">' + user + ' ' + messageTime + message + '</div>';
-    
-    //var $tweet = $('li');
-    
-    //$tweet.text('@' + user + ': ' + tweet.message + ' at ' + tweet.created_at);
-
-
-    
     $(tweetContainer).prependTo($body);
-    index -= 1;
   }
-  return streams.home.length - 1;
+
+  index1 = index2;
+  // while(index >= updatedIndex){
+  //   var tweet = streams.home[index];
+  //   var user = '<span class="user"><strong>' + tweet.user + '</strong></span>';// need to insert a link for a modal for the user
+  //   var messageTime = '<span class="message-time">' + extractTime(tweet.created_at) + '</span>';
+  //   var message = '<div class="message">' + tweet.message + '</div>';
+  //   var tweetContainer = '<div class="tweet-container">' + user + ' ' + messageTime + message + '</div>';
+  //   $(tweetContainer).prependTo($body);
+  //   index -= 1;
+  // }
 
 } 
 
-function checkNewTweets(index) {
-  console.log('checkNewTweets');
-  var updatedIndex = streams.home.length - 1;
-  if (updatedIndex > index) {
-    var numNewTweets = updatedIndex - index;
+function checkNewTweets() {
+  
+  index2 = streams.home.length - 1;
+  if (index2 > index1) {
+    var numNewTweets = index2 - index1;
     if ($('div.show-more').css('display') === 'none') {
       $('div.show-more').show();
-      $('.num-new-tweets').text(numNewTweets);
+      
     }
+    $('#num-new-tweets').text(numNewTweets);
   }
-
 }
