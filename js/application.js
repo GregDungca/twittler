8c08ec90f024146abe2a41c4d9d58c9d2c4b2d7a
GregDungca <greg.dungca@gmail.com>
2015-09-19 11:28:47 -0400

    Working on adding user timeline

--- js/application.js
+++ js/application.js
@@ -1,22 +1,58 @@
-var $body = $('.tweet-list');
+var $tweetList = $('.tweet-list');
 var index1, index2;
-$(document).ready(function(){
-  //$body.html('');    
+$(document).ready(function(){ 
   index1 = 0;
-  index2 = streams.home.length - 1;
+  index2 = streams.home.length;
   getNewTweets();
 
-  setInterval(checkNewTweets, 15000);
+  setInterval(checkNewTweets, 10000);
+
+
   $('button.show-more').on('click', function() {
     $('div.show-more').hide();//optionally animate this
     getNewTweets();
   });
 
-  // var showMore = '<div class="tweet-container">' + '<button class="show-more">Show more</button>' + '</div>';
-  // $(showMore).prependTo($body);
-  // $('.show-more').on('click', function() {
-  //   getNewTweets(index);
-  // })
+  $('.user').on('click', function(event) {
+    var $userTweetList = $('.user-tweet-list');
+    event.preventDefault();
+    $('#user-modal').modal();
+    var userName = $(this).find('a').data('user');
+    var uIndex1 = 0;
+    var uIndex2 = streams.users[userName].length;
+    getNewUserTweets();
+    var intervalCheck = setInterval(checkNewUserTweets, 10000);
+
+    $('#user-modal').on('hide.bs.modal', function() {
+      console.log('Model is hidden');
+      clearInterval(intervalCheck);
+    });
+
+    function getNewUserTweets() {
+      for ( var i = uIndex1; i < uIndex2; i ++ ) {
+        var tweet = streams.users[userName][i];
+        var user = '<span class="user">' + tweet.user + '</span>';
+        var messageTime = '<span class="message-time">' + extractTime(tweet.created_at) + '</span>';
+        var message = '<div class="message">' + tweet.message + '</div>';
+        var tweetContainer = '<div class="tweet-container">' + user + ' ' + messageTime + message + '</div>';
+        $(tweetContainer).prependTo($userTweetList);
+      }
+      uIndex1 = uIndex2;
+    }
+
+    function checkNewUserTweets() {
+        uIndex2 = streams.home.length - 1;
+        if (uIndex2 > uIndex1) {
+          var numNewTweets = uIndex2 - uIndex1;
+          if ($('div.user-show-more').css('display') === 'none') {
+            $('div.user-show-more').show();
+          }
+          $('#num-new-user-tweets').text(numNewTweets);
+        }
+    }
+
+  });
+
 });
 
 
@@ -29,35 +65,24 @@ function extractTime(rawTime) {
 function getNewTweets() {
   for ( var i = index1; i < index2; i ++ ) {
     var tweet = streams.home[i];
-    var user = '<span class="user"><strong>' + tweet.user + '</strong></span>';// need to insert a link for a modal for the user
+    var user = '<span class="user"><a href="#" data-user="' + tweet.user + '"">' + tweet.user + '</a></span>';
     var messageTime = '<span class="message-time">' + extractTime(tweet.created_at) + '</span>';
     var message = '<div class="message">' + tweet.message + '</div>';
     var tweetContainer = '<div class="tweet-container">' + user + ' ' + messageTime + message + '</div>';
-    $(tweetContainer).prependTo($body);
+    $(tweetContainer).prependTo($tweetList);
   }
-
   index1 = index2;
-  // while(index >= updatedIndex){
-  //   var tweet = streams.home[index];
-  //   var user = '<span class="user"><strong>' + tweet.user + '</strong></span>';// need to insert a link for a modal for the user
-  //   var messageTime = '<span class="message-time">' + extractTime(tweet.created_at) + '</span>';
-  //   var message = '<div class="message">' + tweet.message + '</div>';
-  //   var tweetContainer = '<div class="tweet-container">' + user + ' ' + messageTime + message + '</div>';
-  //   $(tweetContainer).prependTo($body);
-  //   index -= 1;
-  // }
-
 } 
 
 function checkNewTweets() {
-  
   index2 = streams.home.length - 1;
   if (index2 > index1) {
     var numNewTweets = index2 - index1;
     if ($('div.show-more').css('display') === 'none') {
       $('div.show-more').show();
-      
     }
     $('#num-new-tweets').text(numNewTweets);
   }
 }
+
+
