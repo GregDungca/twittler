bdbf88e54172f3ba137188c9b03452ae0aa839bc
GregDungca <greg.dungca@gmail.com>
2015-09-19 12:49:15 -0400

    Continued work on user timeline. Need to fix bug in accessing timeline for user name links that are displayed for new tweets

--- js/application.js
+++ js/application.js
@@ -1,53 +1,60 @@
-var $tweetList = $('.tweet-list');
+var $m = $('.main');
 var index1, index2;
 $(document).ready(function(){ 
   index1 = 0;
-  index2 = streams.home.length;
+  index2 = streams.home.length - 1;
   getNewTweets();
 
   setInterval(checkNewTweets, 10000);
 
 
-  $('button.show-more').on('click', function() {
-    $('div.show-more').hide();//optionally animate this
+  $m.find('button.show-more').on('click', function() {
+    $m.find('div.show-more').hide();//optionally animate this
     getNewTweets();
   });
 
   $('.user').on('click', function(event) {
-    var $userTweetList = $('.user-tweet-list');
+    var $um = $('#user-modal');
     event.preventDefault();
-    $('#user-modal').modal();
+    $um.modal();
     var userName = $(this).find('a').data('user');
     var uIndex1 = 0;
-    var uIndex2 = streams.users[userName].length;
+    var uIndex2 = streams.users[userName].length - 1;
+    $um.find('.modal-title').text(userName);
     getNewUserTweets();
     var intervalCheck = setInterval(checkNewUserTweets, 10000);
 
-    $('#user-modal').on('hide.bs.modal', function() {
+    $um.find('button.show-more').on('click', function() {
+      $um.find('div.show-more').hide();//optionally animate this
+      getNewUserTweets();
+    });
+
+    $um.on('hide.bs.modal', function() {
       console.log('Model is hidden');
       clearInterval(intervalCheck);
+      $um.find('.tweet-list').html('');
     });
 
     function getNewUserTweets() {
-      for ( var i = uIndex1; i < uIndex2; i ++ ) {
+      for ( var i = uIndex1; i <= uIndex2; i ++ ) {
         var tweet = streams.users[userName][i];
         var user = '<span class="user">' + tweet.user + '</span>';
         var messageTime = '<span class="message-time">' + extractTime(tweet.created_at) + '</span>';
         var message = '<div class="message">' + tweet.message + '</div>';
         var tweetContainer = '<div class="tweet-container">' + user + ' ' + messageTime + message + '</div>';
-        $(tweetContainer).prependTo($userTweetList);
+        $(tweetContainer).prependTo($um.find('.tweet-list'));
       }
       uIndex1 = uIndex2;
     }
 
     function checkNewUserTweets() {
-        uIndex2 = streams.home.length - 1;
+        uIndex2 = streams.users[userName].length - 1;
         if (uIndex2 > uIndex1) {
           var numNewTweets = uIndex2 - uIndex1;
-          if ($('div.user-show-more').css('display') === 'none') {
-            $('div.user-show-more').show();
+          if ($um.find('div.show-more').css('display') === 'none') {
+            $um.find('div.show-more').show();
           }
-          $('#num-new-user-tweets').text(numNewTweets);
+          $um.find('#num-new-tweets').text(numNewTweets);
         }
     }
 
@@ -63,13 +70,13 @@ function extractTime(rawTime) {
 }
 
 function getNewTweets() {
-  for ( var i = index1; i < index2; i ++ ) {
+  for ( var i = index1; i <= index2; i ++ ) {
     var tweet = streams.home[i];
     var user = '<span class="user"><a href="#" data-user="' + tweet.user + '"">' + tweet.user + '</a></span>';
     var messageTime = '<span class="message-time">' + extractTime(tweet.created_at) + '</span>';
     var message = '<div class="message">' + tweet.message + '</div>';
     var tweetContainer = '<div class="tweet-container">' + user + ' ' + messageTime + message + '</div>';
-    $(tweetContainer).prependTo($tweetList);
+    $(tweetContainer).prependTo($m.find('.tweet-list'));
   }
   index1 = index2;
 } 
@@ -78,10 +85,10 @@ function checkNewTweets() {
   index2 = streams.home.length - 1;
   if (index2 > index1) {
     var numNewTweets = index2 - index1;
-    if ($('div.show-more').css('display') === 'none') {
-      $('div.show-more').show();
+    if ($m.find('div.show-more').css('display') === 'none') {
+      $m.find('div.show-more').show();
     }
-    $('#num-new-tweets').text(numNewTweets);
+    $m.find('#num-new-tweets').text(numNewTweets);
   }
 }
 
